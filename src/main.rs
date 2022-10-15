95f3383c53f4cf38d9fdaca8fa2754acdda3080e
= <=>
2022-10-15 03:52:49 -0500

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,70 @@
-struct User {
-    name: String,
-    is_logged: bool,
-    age: i8,
-    login_count: u64,
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
-struct Color(i32, i32, i32);
+struct IpAdress {
+    adressType: IpAddrKind,
+    adress: String,
+}
+
+enum IppAddrWithType {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quite,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+enum Option<T> {
+    None,
+    Some(T),
+}
 
 fn main() {
-    get_user();
-    let user1 = build_user(String::from("Abdou"), 23);
-    let user2: User = User {
-        name: String::from("Abderahmane Aoufi"),
-        ..user1
+    let home = IppAddrWithType::V4(127, 0, 0, 1);
+    let localhost = IpAdress {
+        adressType: IpAddrKind::V4,
+        adress: String::from("127.0.0.1"),
     };
-    // println!("{:?}", user2.age);
-    let color = Color(129, 141, 155);
-    rectangle();
-    let r1 = Rectangle {
-        width : 33,
-        height : 33
-    };
-    println!("{:?}" , r1);
-    area2(&r1);
+    println!("{}", localhost.adress);
+    let m = Message::Write(String::from("Hello world!"));
+    m.call();
+    let some_char = Some("H");
+    let some_num = Some(12);
+    // let abs: Option<i32> = None;
+    let val = get_currency_in_dinar(500.0 , Currency::Dinar);
+    println!("{}" , val)
 }
 
-fn get_user() -> User {
-    let full_name = String::from("Abderahamane Aoufi");
-    User {
-        name: full_name,
-        is_logged: false,
-        age: 23,
-        login_count: 0,
+impl Message {
+    fn call(&self) {
+        println!("Inside body function!!  ");
     }
 }
 
-fn build_user(full_name: String, age: i8) -> User {
-    User {
-        name: full_name,
-        age,
-        login_count: 1,
-        is_logged: true,
+enum Currency {
+    Dinar,
+    Dollar,
+    Euro,
+}
+
+fn get_currency_in_dinar(value: f32, currency: Currency) -> f32{
+    match(currency) {
+        Currency::Dinar => value,
+        Currency::Dollar => value * 221.00,
+        Currency::Euro => value * 214.00
     }
-}
-
-fn area(dimenstion: (u32, u32)) -> u32 {
-    dimenstion.0 * dimenstion.1
-}
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-fn area2(dimension: &Rectangle) -> u32 {
-    dimension.height * dimension.width
-}
-
-fn rectangle() {
-    let width1: i32 = 450;
-    let height1: i32 = 780;
-    let d = (20, 50);
-    println!("The area of the rectangle is {} square pixels.", area(d));
 }
